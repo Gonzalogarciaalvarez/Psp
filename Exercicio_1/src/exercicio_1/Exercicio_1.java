8b4e59cbf502dbc0d78fd11ad3020d545df16305
Gonzalo <Gonzalo@192.168.0.23>
2018-11-14 18:07:04 +0100

    Commit para subir

---
+++ Exercicio_1/src/exercicio_1/Exercicio_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio_1;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class Exercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Creamos ambos hilos (Nombre Referencia, Tiempo de Interrupcion)
+        
+        Hilo hilo1 = new Hilo("Hilo 1", 1000);
+        Hilo hilo2 = new Hilo("Hilo 2", 6000);
+        
+    }
+    
+}
