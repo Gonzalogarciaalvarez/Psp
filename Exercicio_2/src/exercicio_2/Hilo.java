51ad7ad15afe9c36940ace49bd5a708e99514a87
Gonzalo <Gonzalo@192.168.0.23>
2018-11-14 18:07:19 +0100

    Commit para subir

---
+++ Exercicio_2/src/exercicio_2/Hilo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio_2;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class Hilo extends Thread{
+        
+    private final String nombre;
+    
+    public Hilo(String nombre){
+        
+        this.nombre = nombre;
+        this.start();
+    }
+    
+    @Override public void run() {
+
+        for(int i = 0; i < 5; i++){
+            
+            System.out.println(i + " - " + nombre);
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {}
+        }
+    }
+}
