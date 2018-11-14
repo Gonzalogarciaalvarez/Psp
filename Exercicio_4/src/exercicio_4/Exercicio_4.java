48bb36dcb92ea0e654cc3e1c1a8320255503c0a1
Gonzalo <Gonzalo@192.168.0.23>
2018-11-14 18:07:32 +0100

    Commit para subir

---
+++ Exercicio_4/src/exercicio_4/Exercicio_4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio_4;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class Exercicio_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Hilo(1);
+        new Hilo(2);
+        new Hilo(3);
+    }
+    
+}
